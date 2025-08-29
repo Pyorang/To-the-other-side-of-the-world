425c1b364e02a1f1cd072efafedb271b6b4902e7
Lee Seung Been <144720218+Pyorang@users.noreply.github.com>
2025-08-29 19:20:59 +0900

    Feat : SceneLoader 구현

---
+++ Assets/Scripts/Common/SceneLoader.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum ESceneType
+{
+    Title,
+    Lobby,
+    InGame
+}
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(ESceneType sceneType)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneType.ToString());
+    }
+
+    public void ReloadScene()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public AsyncOperation LoadSceneAsync(ESceneType sceneType)
+    {
+        Time.timeScale = 1f;
+        return SceneManager.LoadSceneAsync(sceneType.ToString());
+    }
+}
