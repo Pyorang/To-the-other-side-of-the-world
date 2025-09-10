afec8fe617e54c33f44fa2698fb1cb88dceac098
minimiYakgwa <qorudalswl@naver.com>
2025-09-10 21:30:33 +0900

    Refactor : LobbyUIController prefab 하이어라기 배치 및 상점 버튼 텍스트 수정, ShopUI -> MissionShopUI로 스크립트 변경 및 최적화

---
+++ Assets/Scripts/Common/UI/MissionShopUI.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+enum UiType
+{
+    DM = 0,
+    CM = 1,
+    Shop = 2
+}
+public class MissionShopUI : BaseUI
+{
+
+    [SerializeField] private List<GameObject> uis = new List<GameObject>();
+    [SerializeField] private List<Button> btns = new List<Button>();
+
+    private AchievementModel[] _dailyAchievements;
+    private CharacterModel[] _characterModel;
+
+    private int currentUiIndex = 0;
+
+    public override void SetData(BaseUIData data)
+    {
+        base.SetData(data);
+
+        // UserDataManager에서 접속날짜와 기록된 날짜를 비교하여 미션 초기화 여부 결정
+
+        //ShopUI, DMUI, CMUI, CSUI 분리해서 스크립트 구성해야겠다.
+        //얘네들은 별개의 UI 창이라고 생각하고 하는게 맞는듯
+
+        // DM 불러오기
+        /*for (int i = 0; i < 4; i++)
+        {
+            _dailyAchievements[i] = DataTableManager.Instance.GetAchievementData($"DM_{i + 1}");
+        }*/
+
+        // 상점에 필요한 캐릭터 데이터 불러오기
+        //_characterModel = DataTableManager.Instance.GetAllChracterData();
+
+
+    }
+    public void OnClickShowDailyMissionUI()
+    {
+        ChangeUI((int)UiType.DM);
+    }
+
+    public void OnClickShowChallengeMissionUI()
+    {
+        ChangeUI((int)UiType.CM);
+    }
+
+    public void OnClickShowShopUI()
+    {
+        ChangeUI((int)UiType.Shop);
+    }
+
+    public void OnClickChangeShopPage()
+    {
+        Debug.Log($"{GetType()}::{nameof(OnClickChangeShopPage)}");
+    }
+
+    private void ChangeUI(int UiIndex)
+    {
+        uis[currentUiIndex].SetActive(false);
+        btns[currentUiIndex].GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        btns[currentUiIndex].interactable = true;
+
+        uis[UiIndex].SetActive(true);
+        btns[UiIndex].GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        btns[UiIndex].interactable = false;
+
+        currentUiIndex = UiIndex;
+    }
+
+}
--- Assets/Scripts/Common/UI/ShopUI.cs
+++ Assets/Scripts/Common/UI/ShopUI.cs
@@ -1,6 +1,11 @@
+using System.Linq;
+using System.Runtime.CompilerServices;
+using NUnit.Framework;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class ShopUIData : BaseUIData
 {
@@ -9,9 +14,6 @@ public class ShopUIData : BaseUIData
 }
 public class ShopUI : BaseUI
 {
-    [SerializeField] private TextMeshProUGUI _menuNameText;
-    //[SerializeField] private TextMeshProUGUI[]  _dailyMissionTexts;
-    //[SerializeField] private TextMeshProUGUI _dailyMissionProgressText;
     [SerializeField] private GameObject _dailyMissionUI;
     [SerializeField] private GameObject _challengeUI;
     [SerializeField] private GameObject _characterShopUI;
@@ -19,6 +21,13 @@ public class ShopUI : BaseUI
     [SerializeField] private Button _challengeMissionButton;
     [SerializeField] private Button _characterShopButton;
 
+    [SerializeField] private List<GameObject> uis= new List<GameObject>();
+    [SerializeField] private List<Button> btns = new List<Button>();
+
+    private AchievementModel[] _dailyAchievements;
+    private CharacterModel[] _characterModel;
+
+    private int currentUiIndex = 0;
 
     public override void SetData(BaseUIData data)
     {
@@ -28,40 +37,71 @@ public class ShopUI : BaseUI
 
         //ShopUI, DMUI, CMUI, CSUI 분리해서 스크립트 구성해야겠다.
         //얘네들은 별개의 UI 창이라고 생각하고 하는게 맞는듯
-        
+
+        // DM 불러오기
+        /*for (int i=0; i<4; i++)
+        {
+            _dailyAchievements[i] = DataTableManager.Instance.GetAchievementData($"DM_{i + 1}");
+        }*/
+
+        // 상점에 필요한 캐릭터 데이터 불러오기
+        //_characterModel = DataTableManager.Instance.GetAllChracterData();
+
+
     }
     public void OnClickShowDailyMissionUI()
     {
-        _dailyMissionUI.SetActive(true);
+        ChangeUI((int)UiType.DM);
+        /*_dailyMissionUI.SetActive(true);
         _challengeUI.SetActive(false);
         _characterShopUI.SetActive(false);
         _dailyMissionButton.GetComponent<Image>().color = new Color(1, 1, 1, 0);
         _challengeMissionButton.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-        _characterShopButton.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        _characterShopButton.GetComponent<Image>().color = new Color(1, 1, 1, 1);*/
 
     }
 
     public void OnClickShowChallengeMissionUI()
     {
-        _dailyMissionUI.SetActive(false);
+        ChangeUI((int)UiType.CM);
+        /*_dailyMissionUI.SetActive(false);
         _challengeUI.SetActive(true);
         _characterShopUI.SetActive(false);
         _dailyMissionButton.GetComponent<Image>().color = new Color(1, 1, 1, 1);
         _challengeMissionButton.GetComponent<Image>().color = new Color(1, 1, 1, 0);
-        _characterShopButton.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        _characterShopButton.GetComponent<Image>().color = new Color(1, 1, 1, 1);*/
     }
 
-    public void OnClickShowChracterShopUI()
+    public void OnClickShowShopUI()
     {
-        _dailyMissionUI.SetActive(false);
+        ChangeUI((int)UiType.Shop);
+        /*_dailyMissionUI.SetActive(false);
         _challengeUI.SetActive(false);
         _characterShopUI.SetActive(true);
         _dailyMissionButton.GetComponent<Image>().color = new Color(1, 1, 1, 1);
         _challengeMissionButton.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-        _characterShopButton.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        _characterShopButton.GetComponent<Image>().color = new Color(1, 1, 1, 0);*/
+
+
     }
 
-    
+    public void OnClickChangeShopPage()
+    {
+        Debug.Log($"{GetType()}::{nameof(OnClickChangeShopPage)}");
+
+    }
+
+    private void ChangeUI(int UiIndex)
+    {
+        Debug.Log($"curentUiIndex : {currentUiIndex}, UiIndex : {UiIndex}");
+        uis[currentUiIndex].SetActive(false);
+        btns[currentUiIndex].GetComponent<Image>().color = new Color(1, 1, 1, 1);
+
+        uis[UiIndex].SetActive(true);
+        btns[UiIndex].GetComponent<Image>().color = new Color(1, 1, 1, 0);
+
+        currentUiIndex = UiIndex;
+    }
 
 
 
