027d22b50e518c15da3443fba831caa788094515
이승빈 <144720218+Pyorang@users.noreply.github.com>
2025-10-31 17:35:43 +0900

    feature : 테이머 스킬 구현 완료

---
+++ Assets/Scripts/InGame/SkillStrategy/IAutoUseSkill.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface IAutoUseSkill
+{
+    public void SetCoolTime();
+}
--- Assets/Scripts/InGame/SkillStrategy/Skill_3Strategy.cs
+++ Assets/Scripts/InGame/SkillStrategy/Skill_3Strategy.cs
@@ -1,9 +1,51 @@
 using UnityEngine;
 
-public class Skill_3Strategy : ISkillStrategy
+public class Skill_3Strategy : ISkillStrategy, IAutoUseSkill
 {
+    private static readonly int SkillCoolTime = 20;
+    public int CurrentCoolTime = 0;
+
+    public Skill_3Strategy()
+    {
+        InGameManager.OnGameStageCleared += CoolTimeReduce;
+        InGameManager.Instance.HasAutoShield = true;
+    }
+
+    public void UnequipCharacterSkill()
+    {
+        InGameManager.OnGameStageCleared -= CoolTimeReduce;
+    }
+
     public void UseSkill()
     {
-        Debug.Log("스킬 사용3");
+        MakeAutoShield();
+    }
+
+    public void CoolTimeReduce()
+    {
+        if(!InGameManager.Instance.HasAutoShield)
+        {
+            if (CurrentCoolTime > 0)
+            {
+                CurrentCoolTime--;
+                InGameManager.Instance.UpdateSkillCoolTime(CurrentCoolTime, SkillCoolTime);
+            }
+
+            if (CurrentCoolTime == 0)
+            {
+                UseSkill();
+            }
+        }
+    }
+
+    public void MakeAutoShield()
+    {
+        InGameManager.Instance.HasAutoShield = true;
+    }
+
+    public void SetCoolTime()
+    {
+        CurrentCoolTime = SkillCoolTime;
+        InGameManager.Instance.UpdateSkillCoolTime(CurrentCoolTime, SkillCoolTime);
     }
 }
